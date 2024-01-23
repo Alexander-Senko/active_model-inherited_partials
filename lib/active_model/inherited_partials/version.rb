Alexander-Senko/organizer

Framework: generated a new Rails Engine
* Rails 7.1;
* Ruby 3.2.

lib/organizer/version.rb
@@ -1,0 +1,3 @@
+module Organizer
+  VERSION = "0.1.0"
+end

Alexander-Senko/active_model-inherited_partials

lib/active_model/inherited_partials/version.rb
@@ -1,0 +1,5 @@
+module ActiveModel
+  module InheritedPartials
+    VERSION = "0.1.0"
+  end
+end